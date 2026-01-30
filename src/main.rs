c0529aafe34634266bdbc3cff96d262278e81fcf
AzureZee <azureze@qq.com>
2026-01-31 01:38:11 +0800

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,165 +1,46 @@
 use std::fmt::Write as _;
 use std::fs;
-
 use std::io::Write as _;
-use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub};
 
+mod vec3;
+use vec3::*;
 fn main() {
     let img_w = 256;
     let img_h = 256;
 
-    let mut canvas = String::new();
-    let _ = write!(canvas, "P3\n{} {}\n255\n", img_w, img_h);
+    let mut buf = String::new();
+    let _ = write!(buf, "P3\n{} {}\n255\n", img_w, img_h);
 
     for j in 0..img_h {
         eprint!("\rprogress: {}/{} ", j + 1, img_h);
         let _ = std::io::stderr().flush();
 
         for i in 0..img_w {
-            let pixel = Pixel {
-                e: [
-                    double(i) / double(img_w - 1),
-                    double(j) / double(img_h - 1),
-                    0.0,
-                ],
-            };
-            put_pixel(&mut canvas, pixel);
+            let color = RGB::new([
+                double(i) / double(img_w - 1),
+                double(j) / double(img_h - 1),
+                0.0,
+            ]);
+            color.write_color(&mut buf);
         }
     }
     let mut file = fs::File::create("img.ppm").unwrap();
-    let _ = file.write_all(canvas.as_bytes());
+    let _ = file.write_all(buf.as_bytes());
 }
 
 fn double(i: i32) -> Double {
     i as Double
 }
 
-pub fn put_pixel(canvas: &mut String, pixel: Pixel) {
-    fn translate(byte: Double) -> UInt {
-        let n = 255.999;
-        (n * byte) as UInt
-    }
-    let [r, g, b] = [
-        translate(pixel.x()),
-        translate(pixel.y()),
-        translate(pixel.z()),
-    ];
-    let _ = writeln!(canvas, "{} {} {}", r, g, b);
-}
-
-type Point = Vec3;
-type Pixel = Vec3;
-type UInt = u64;
 type Double = f64;
 
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub struct Vec3 {
-    pub e: [Double; 3],
+pub struct Ray {
+    origin: Point3,
+    direction: Vector3,
 }
 
-impl Vec3 {
-    pub fn x(&self) -> Double {
-        self.e[0]
-    }
-    pub fn y(&self) -> Double {
-        self.e[1]
-    }
-    pub fn z(&self) -> Double {
-        self.e[2]
-    }
-    pub fn dot(&self, vec_b: &Vec3) -> Double {
-        (*self * vec_b).e.iter().sum()
-    }
-    pub fn cross(&self, vec_b: &Vec3) -> Self {
-        let vec_a = self;
-        let c_z = vec_a.x() * vec_b.y() - vec_a.y() * vec_b.x();
-        let c_y = vec_a.z() * vec_b.x() - vec_a.x() * vec_b.z();
-        let c_x = vec_a.y() * vec_b.z() - vec_a.z() * vec_b.y();
-        Self { e: [c_x, c_y, c_z] }
-    }
-    pub fn unit_vector(&self) -> Self {
-        *self / self.len()
-    }
-    pub fn len(&self) -> Double {
-        self.len_squared().sqrt()
-    }
-    pub fn len_squared(&self) -> Double {
-        (*self * self).e.iter().sum()
-    }
-}
-
-impl Add for Vec3 {
-    type Output = Self;
-    fn add(self, rhs: Self) -> Self::Output {
-        Self {
-            e: [self.x() + rhs.x(), self.y() + rhs.y(), self.z() + rhs.z()],
-        }
-    }
-}
-impl AddAssign for Vec3 {
-    fn add_assign(&mut self, rhs: Self) {
-        *self = *self + rhs;
-    }
-}
-impl Sub for Vec3 {
-    type Output = Self;
-    fn sub(self, rhs: Self) -> Self::Output {
-        Self {
-            e: [self.x() - rhs.x(), self.y() - rhs.y(), self.z() - rhs.z()],
-        }
-    }
-}
-impl Mul for Vec3 {
-    type Output = Self;
-
-    fn mul(self, rhs: Self) -> Self::Output {
-        Self {
-            e: [self.x() * rhs.x(), self.y() * rhs.y(), self.z() * rhs.z()],
-        }
-    }
-}
-impl Mul<&Vec3> for Vec3 {
-    type Output = Self;
-
-    fn mul(self, rhs: &Vec3) -> Self::Output {
-        Self {
-            e: [self.x() * rhs.x(), self.y() * rhs.y(), self.z() * rhs.z()],
-        }
-    }
-}
-impl Mul<Double> for Vec3 {
-    type Output = Self;
-
-    fn mul(self, rhs: Double) -> Self::Output {
-        Self {
-            e: [self.x() * rhs, self.y() * rhs, self.z() * rhs],
-        }
-    }
-}
-
-impl MulAssign<Double> for Vec3 {
-    fn mul_assign(&mut self, rhs: Double) {
-        *self = *self * rhs;
-    }
-}
-impl Div<Double> for Vec3 {
-    type Output = Self;
-
-    fn div(self, rhs: Double) -> Self::Output {
-        self * (1.0 / rhs)
-    }
-}
-impl DivAssign<Double> for Vec3 {
-    fn div_assign(&mut self, rhs: Double) {
-        *self = *self / rhs;
-    }
-}
-impl Neg for Vec3 {
-    type Output = Self;
-
-    fn neg(self) -> Self::Output {
-        Self {
-            e: [-self.x(), -self.y(), -self.z()],
-        }
+impl Ray {
+    pub fn at(&self, t: Double) -> Point3 {
+        self.origin + self.direction * t
     }
 }
---
+++ src/vec3.rs
@@ -0,0 +1,219 @@
+use std::fmt::Write as _;
+use std::{
+    marker::PhantomData,
+    ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub},
+};
+type Number = f64;
+
+// type Coordinates = [f64;3];
+// type Magnitude = f64;
+pub type Point3 = Vec3<Point>;
+pub type Vector3 = Vec3<Vector>;
+pub type RGB = Vec3<Color>;
+
+#[derive(PartialEq, Debug)]
+pub struct Color;
+#[derive(PartialEq, Debug)]
+pub struct Point;
+#[derive(PartialEq, Debug)]
+pub struct Vector;
+
+// https://gabrielgambetta.com/computer-graphics-from-scratch/A0-linear-algebra.html
+
+#[derive(Debug, PartialEq)]
+pub struct Vec3<T> {
+    pub arr: [Number; 3],
+    _marker: PhantomData<T>,
+}
+
+impl<T> Copy for Vec3<T> {}
+impl<T> Clone for Vec3<T> {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+impl HasCoordinates for Point {}
+impl HasCoordinates for Vector {}
+pub trait HasCoordinates {}
+impl<T> Vec3<T> {
+    pub fn new(arr: [Number; 3]) -> Self {
+        Self {
+            arr,
+            _marker: PhantomData,
+        }
+    }
+}
+impl<T: HasCoordinates> Vec3<T> {
+    pub fn x(&self) -> Number {
+        self.arr[0]
+    }
+    pub fn y(&self) -> Number {
+        self.arr[1]
+    }
+    pub fn z(&self) -> Number {
+        self.arr[2]
+    }
+}
+impl RGB {
+    pub fn r(&self) -> Number {
+        self.arr[0]
+    }
+    pub fn g(&self) -> Number {
+        self.arr[1]
+    }
+    pub fn b(&self) -> Number {
+        self.arr[2]
+    }
+    pub fn write_color(&self, buf: &mut String) {
+        fn translate(old: [Number; 3]) -> [u64; 3] {
+            let n = 255.999;
+            let mut new = [0; 3];
+            for i in 0..old.len() - 1 {
+                new[i] = (n * old[i]) as u64;
+            }
+            new
+        }
+        let [r, g, b] = translate(self.arr);
+        let _ = writeln!(buf, "{} {} {}", r, g, b);
+    }
+}
+
+impl Vector3 {
+    pub fn dot(&self, other: Vector3) -> Number {
+        // dot product: multiply each of the coordinates and add each of the coordinates product
+        // V dot W = V_x * W_x + V_y * W_y + V_z * W_z
+
+        // This suggests another way to compute the length of a vector, as the square root of its dot product with itself
+        // V dot V = V_x^2 + V_y^2 + V_z^2 = V_len^2
+        (*self * other).arr.iter().sum()
+    }
+
+    pub fn cross(&self, other: Vector3) -> Self {
+        // The cross product between two vectors gives you another vector,it perpendicular to both of them
+        // R=V cross W
+        // R_x=V_y * W_z − V_z * W_y
+        // R_y=V_z * W_x − V_x * W_z
+        // R_z=V_x * W_y − V_y * W_x
+        let v_xyz = self.clone();
+        let w_xyz = other;
+        let mut v_zxy = v_xyz;
+        let mut w_zxy = w_xyz;
+        let mut v_yzx = v_zxy;
+        let mut w_yzx = w_zxy;
+
+        // [(x,y),z]=>[z,(x,y)]
+        v_zxy.arr.rotate_left(2);
+        w_zxy.arr.rotate_left(2);
+        // [x,(y,z)]=>[(y,z),x]
+        v_yzx.arr.rotate_right(2);
+        w_yzx.arr.rotate_right(2);
+
+        v_yzx * w_zxy - v_zxy * w_yzx
+    }
+    pub fn unit_vector(&self) -> Self {
+        // normalize a vector: into a unit vector, divide the vector by its length
+
+        *self / self.len()
+    }
+    pub fn len(&self) -> Number {
+        // vector magnitude (also called the length or norm).
+        // vec(x,y,z), vec_len= sqrt(x^2 + y^2 + z^2)
+        self.len_squared().sqrt()
+    }
+    pub fn len_squared(&self) -> Number {
+        self.dot(*self)
+    }
+    fn _cross(&self, vec_b: Vector3) -> Self {
+        let vec_a = self;
+        let c_z = vec_a.x() * vec_b.y() - vec_a.y() * vec_b.x();
+        let c_y = vec_a.z() * vec_b.x() - vec_a.x() * vec_b.z();
+        let c_x = vec_a.y() * vec_b.z() - vec_a.z() * vec_b.y();
+        Self {
+            arr: [c_x, c_y, c_z],
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<T: HasCoordinates> Add<Vector3> for Vec3<T> {
+    type Output = Self;
+    // add a vector to a point and get a new point
+    // or
+    // add two vectors and get a new vector
+    fn add(self, rhs: Vector3) -> Self::Output {
+        Self {
+            arr: [self.x() + rhs.x(), self.y() + rhs.y(), self.z() + rhs.z()],
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<T: HasCoordinates> AddAssign<Vector3> for Vec3<T> {
+    fn add_assign(&mut self, rhs: Vector3) {
+        *self = *self + rhs;
+    }
+}
+impl<T: HasCoordinates> Sub for Vec3<T> {
+    // subtract two points and get a vector, subtract each of the coordinates separately
+
+    type Output = Vector3;
+    fn sub(self, rhs: Self) -> Self::Output {
+        Vector3 {
+            arr: [self.x() - rhs.x(), self.y() - rhs.y(), self.z() - rhs.z()],
+            _marker: PhantomData,
+        }
+    }
+}
+impl Mul for Vector3 {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self {
+            arr: [self.x() * rhs.x(), self.y() * rhs.y(), self.z() * rhs.z()],
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl Mul<Number> for Vector3 {
+    type Output = Self;
+    // multiply a vector by a number k and get the scalar product.
+    // This makes the vector shorter or longer
+
+    fn mul(self, rhs: Number) -> Self::Output {
+        Self {
+            arr: [self.x() * rhs, self.y() * rhs, self.z() * rhs],
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl MulAssign<Number> for Vector3 {
+    fn mul_assign(&mut self, rhs: Number) {
+        *self = *self * rhs;
+    }
+}
+impl Div<Number> for Vector3 {
+    type Output = Self;
+    // divide a vector by a number k.
+    // dividing by k is equivalent to multiplying by 1/k
+
+    fn div(self, rhs: Number) -> Self::Output {
+        self * (1.0 / rhs)
+    }
+}
+impl DivAssign<Number> for Vector3 {
+    fn div_assign(&mut self, rhs: Number) {
+        *self = *self / rhs;
+    }
+}
+impl Neg for Vector3 {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        Self {
+            arr: [-self.x(), -self.y(), -self.z()],
+            _marker: PhantomData,
+        }
+    }
+}
